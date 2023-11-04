84f3373761d1c2e705077b2086428e3718ab714f
Ayomikun Oloyede <o.ayomikun83@gmail.com>
2023-11-05 00:34:39 +0100

    feature: Notification features implemented

--- src/shared/services/queues/email.queue.ts
+++ src/shared/services/queues/email.queue.ts
@@ -6,6 +6,11 @@ class EmailQueue extends BaseQueue {
   constructor() {
     super('emails');
     this.processJob('forgotPasswordEmail', 5, emailWorker.adNotificationEmail);
+    this.processJob('commentsEmail', 5, emailWorker.adNotificationEmail);
+    this.processJob('followersEmail', 5, emailWorker.adNotificationEmail);
+    this.processJob('reactionsEmail', 5, emailWorker.adNotificationEmail);
+    this.processJob('directMessageEmail', 5, emailWorker.adNotificationEmail);
+    this.processJob('changePassword', 5, emailWorker.adNotificationEmail);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
